6dc4b1da2da68ec28805947883c294442d2d3ad5
tony.yang <tony.yang@bwoil.com>
2019-05-08 10:47:10 +0800

    upload icon svg file

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -40,117 +40,124 @@ export default class SignInScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <ImageBackground
+        {/* <ImageBackground
           source={require('../../assets/images/background.png')}
           resizeMode="cover"
-          style={styles.bgImage}>
+          style={styles.bgImage}> */}
+
+        <View style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <View style={{
+            height: '40%',
+            // flex: 1, alignItems: 'center', justifyContent: 'center' 
+          }}>
+            <Animated.Image resizeMode="contain" style={{ height: 90 }} source={require('../../assets/images/white-logo.png')} />
+          </View>
+
 
           <View style={{
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-            <View style={{ 
-              height: '40%',
-              // flex: 1, alignItems: 'center', justifyContent: 'center' 
-              }}>
-              <Animated.Image resizeMode="contain" style={{ height: 90 }} source={require('../../assets/images/white-logo.png')} />
-            </View>
+            paddingBottom: 10,
+            // flex: 1, alignSelf: 'center' 
+          }}  >
 
+            <Icon name="Tbao" size={32} color="#FC641F" />
+            <Icon name="JD6" size={32} color="#CB3F20" />
+            <Icon name="Tmall2" size={32} color="#CB3F20" />
+            <Icon name="Tmall6" size={32} color="#CB3F20" />
+            <Icon name="explore" size={32} color="#CB3F20" />
+            <Icon name="google-plus2" size={32} color="#3B5998" />
 
-            <View style={{ 
-              paddingBottom :10,
-              // flex: 1, alignSelf: 'center' 
-              }}  >
-              <Icon name="google-plus" size={24} color="#CB3F20"  />
-              <Text>Please login with below</Text>
-            </View>
-
-            <View style={{
-            
-              // flex: 1,
-              // alignItems: 'flex-start',
-              // justifyContent: 'flex-start',
-              // alignSelf: 'center',
-              // paddingBottom: Platform.OS === 'android' ? 30 : 0,
-            }}>
-            
-              <TouchableOpacity style={{
-                height: 45,
-                width: 250,
-                marginBottom: 8,
-                borderRadius: 5,
-                backgroundColor: "#CB3F20",
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }} onPress={this._signInAsync} activeOpacity={0.5}>
-                <View style={{
-                  justifyContent: "center",
-                  alignItems: 'center',
-                  padding: 5,
-                  borderRadius: 5,
-                  width: '20%',
-                  backgroundColor: Color("#CB3F20").lighten(0.3)
-                }}>
-                  <Image style={{
-                    resizeMode: 'contain',
-                    width: "95%",
-                    height: "95%",
-                  }} source={{ uri: 'https://png.icons8.com/google/androidL/40/FFFFFF' }} />
-                </View>
-
-                <View style={{
-                  width: '80%',
-                  alignSelf: 'center',
-                  justifyContent: "center", alignItems: 'center', alignContent: 'center',
-                }}>
-                  <Text style={{
-                    color: 'white',
-                    justifyContent: "center", alignItems: 'center', alignContent: 'center',
-                  }}>Sign in with google</Text>
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity style={{
-                height: 45,
-                width: 250,
-                marginBottom: 8,
-                borderRadius: 5,
-                backgroundColor: "#3B5998",
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }} onPress={this._signInAsync} activeOpacity={0.5}>
-                <View style={{
-                  justifyContent: "center",
-                  alignItems: 'center',
-                  padding: 5,
-                  borderRadius: 5,
-                  width: '20%',
-                  backgroundColor: Color("#3B5998").lighten(0.3)
-                }}>
-                  <Image style={{
-                    resizeMode: 'contain',
-                    width: "95%",
-                    height: "95%",
-                  }} source={{ uri: 'https://png.icons8.com/facebook/androidL/40/FFFFFF' }} />
-                </View>
-
-                <View style={{
-                  width: '80%',
-                  alignSelf: 'center',
-                  justifyContent: "center", alignItems: 'center', alignContent: 'center',
-                }}>
-                  <Text style={{
-                    color: 'white',
-                    justifyContent: "center", alignItems: 'center', alignContent: 'center',
-                  }}>Sign in with facebook</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
+            <Text>Please login with below</Text>
           </View>
-        </ImageBackground>
+
+          <View style={{
+
+            // flex: 1,
+            // alignItems: 'flex-start',
+            // justifyContent: 'flex-start',
+            // alignSelf: 'center',
+            // paddingBottom: Platform.OS === 'android' ? 30 : 0,
+          }}>
+
+            <TouchableOpacity style={{
+              height: 45,
+              width: 250,
+              marginBottom: 8,
+              borderRadius: 5,
+              backgroundColor: "#CB3F20",
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }} onPress={this._signInAsync} activeOpacity={0.5}>
+              <View style={{
+                justifyContent: "center",
+                alignItems: 'center',
+                padding: 5,
+                borderRadius: 5,
+                width: '20%',
+                backgroundColor: Color("#CB3F20").lighten(0.3)
+              }}>
+                <Image style={{
+                  resizeMode: 'contain',
+                  width: "95%",
+                  height: "95%",
+                }} source={{ uri: 'https://png.icons8.com/google/androidL/40/FFFFFF' }} />
+              </View>
+
+              <View style={{
+                width: '80%',
+                alignSelf: 'center',
+                justifyContent: "center", alignItems: 'center', alignContent: 'center',
+              }}>
+                <Text style={{
+                  color: 'white',
+                  justifyContent: "center", alignItems: 'center', alignContent: 'center',
+                }}>Sign in with google</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={{
+              height: 45,
+              width: 250,
+              marginBottom: 8,
+              borderRadius: 5,
+              backgroundColor: "#3B5998",
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }} onPress={this._signInAsync} activeOpacity={0.5}>
+              <View style={{
+                justifyContent: "center",
+                alignItems: 'center',
+                padding: 5,
+                borderRadius: 5,
+                width: '20%',
+                backgroundColor: Color("#3B5998").lighten(0.3)
+              }}>
+                <Image style={{
+                  resizeMode: 'contain',
+                  width: "95%",
+                  height: "95%",
+                }} source={{ uri: 'https://png.icons8.com/facebook/androidL/40/FFFFFF' }} />
+              </View>
+
+              <View style={{
+                width: '80%',
+                alignSelf: 'center',
+                justifyContent: "center", alignItems: 'center', alignContent: 'center',
+              }}>
+                <Text style={{
+                  color: 'white',
+                  justifyContent: "center", alignItems: 'center', alignContent: 'center',
+                }}>Sign in with facebook</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        {/* </ImageBackground> */}
       </View>
     );
   }
